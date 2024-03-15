baa0ec6fb362b639cd9c6803f4f8e5a49d3b5786
Arkadiusz Torun <arek.torun12@gmail.com>
2024-03-15 21:29:30 +0100

    added wraper component and changed properties for styled

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,23 +1,20 @@
-import { Title, Img, Content, Caption, Name, Description, Button, ButtonName, ButtonRectangle } from "./styled";
-import icon from "../../images/mail-icon.svg";
-import profileImage from "../../images/Arek.png";
+import { Wrapper, Img, Content, Caption, Name, Description, Button, ButtonName, ButtonIcon } from "./styled";
+import icon from "./mail-icon.svg";
+import profileImage from "./Arek.png";
 
-const Header = ({ caption, name, description, extraText}) => {
-
-    return(
-        <Title>
-        <Img src={profileImage} alt="Arkadiusz Toruń" />
-        <Content>
-          <Caption>{caption}</Caption>
-          <Name>{name}</Name>
-          <Description>{description}</Description>
-          <Button>
-            <ButtonRectangle src={icon} />
-            <ButtonName>{extraText}</ButtonName>
-          </Button>
-        </Content>
-      </Title>
-    )
-};
+const Header = ({ caption, name, description, extraText, email }) => (
+  <Wrapper>
+    <Img src={profileImage} alt="Arkadiusz Toruń" />
+    <Content>
+      <Caption>{caption}</Caption>
+      <Name>{name}</Name>
+      <Description>{description}</Description>
+      <Button>
+        <ButtonIcon src={icon} />
+        <ButtonName href={`mailto:${email}`} title={email}>{extraText}</ButtonName>
+      </Button>
+    </Content>
+  </Wrapper>
+);
 
 export default Header;
--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -1,61 +1,96 @@
 import styled from "styled-components";
 
-export const Title = styled.article`
+export const Wrapper = styled.article`
     display: grid;
     grid-template-columns: auto 1fr;
     grid-gap: 72px;
     margin: 119px auto ;
     max-width: 1089px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+        grid-gap: 32px;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        grid-gap: 12px;
+        grid-template-columns: 1fr;
+    }
 `;
 
 export const Img = styled.img`
-    width: 398px;
-    height: 398px;
-    border-radius: 60%;
+    width: 30vw;
+    max-width: 398px;
+    border-radius: 50%;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        width: 132px;
+    }
 `;
 export const Content = styled.div``;
 
-export const Caption = styled.p`
+export const Caption = styled.div`
     font-weight: 700;
     font-size: 12px;
-    color: ${({theme}) => theme.color.slategray};
+    color: ${({ theme }) => theme.colors.textPrimary};
     margin-top: 64px;
+    letter-spacing: initial;
 `;
 
 export const Name = styled.h1`
     font-weight: 900;
     font-size: 38px;
     margin: 12px 0 12px 0;
+    color: ${({ theme }) => theme.colors.textPrimary};
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: 22px;
+        margin-top: 8px;
+    }
 `;
 
 export const Description = styled.h1`
     font-weight: 400;
     font-size: 20px;
-    color: ${({theme}) => theme.color.slategray};
+    color: ${({ theme }) => theme.color.site.text};
     margin: 23px 0 32px 0;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+        margin-top: 16px
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: 16px;
+        max-width: 570px;
+    }
 `;
 
 export const Button = styled.button`
     margin-bottom: 0 0 73px 0;
-    padding: 12px 16px 12px 16px;
-    border: 1px solid ${({theme}) => theme.color.iron};
-    background: ${({theme}) => theme.color.scienceblue};
-    border-radius: 4px;
-    width: 154px;
-    height: 49px;
+    padding: 12px 16px;
+    border: 1px solid ${({ theme }) => theme.colors.buttonLink.border};
+    background: ${({ theme }) => theme.colors.primary};
+    border-radius: ${({ theme }) => theme.borderRadiusSmall};
     font-weight: 600;
     font-size: 20px;
-    color: ${({theme}) => theme.color.white};
-    display: flex;
+    color: ${({ theme }) => theme.color.white};
+    display: inline-flex;
     align-items: center;
+    transition: box-shadow 0.3s;
 
+    &:hover {
+        box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.buttonLink.shadow}
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: 18px;
+    }
 `;
 
 export const ButtonName = styled.div`
     padding-left: 16px;
 `;
 
-export const ButtonRectangle = styled.img`
+export const ButtonIcon = styled.img`
     width: 24px;
     height: 24px;
 `;
