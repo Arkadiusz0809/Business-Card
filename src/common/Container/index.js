41287d8eb44199ab98dbdbd07b399af3cc641717
Arkadiusz Torun <arek.torun12@gmail.com>
2024-03-15 23:40:49 +0100

    removed background

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     margin: auto;
-    background: ${({ theme }) => theme.colors.site.background};
 
     @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         padding: 8px; 
