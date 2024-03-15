25dcb590fa7ff5f13e3acb21057f03b6c45f40a7
Arkadiusz Torun <arek.torun12@gmail.com>
2024-03-15 21:35:44 +0100

    added App

---
+++ src/core/App/index.js
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux"
+import { selectIsDarkTheme } from "../../common/themeSlice"
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
+import { Normalize } from "styled-normalize";
+import { themeDark, themeLight } from "./theme";
+import PersonalHomepage from "../../App";
+
+
+export const App = () => {
+    const isDarkTheme = useSelector(selectIsDarkTheme);
+
+    return (
+        <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+            <Normalize />
+            <GlobalStyle />
+            <PersonalHomepage />
+        </ThemeProvider>
+    );
+}
+
+export default App;
