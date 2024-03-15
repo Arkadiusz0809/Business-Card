26c8645db37781d5a1a1988aabd2390a3de1961e
Arkadiusz Torun <arek.torun12@gmail.com>
2024-03-15 21:27:07 +0100

    replaced to App folder

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -12,7 +12,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     padding: 16px;
     font-family: 'Inter', sans-serif;
+    transition: background 0.3s;
     color: ${({ theme }) => theme.color.black};
+    font-size: 18px;
+    letter-spacing: 0.05em;
+    word-break: break-word;
+    padding-bottom: 108px;
 
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+      padding-bottom: 32;
+    }
   }
 `;
