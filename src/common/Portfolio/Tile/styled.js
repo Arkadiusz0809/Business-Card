afea4aa87a7811785aafeb6758b573695e84fb12
Arkadiusz Torun <arek.torun12@gmail.com>
2024-03-15 21:32:50 +0100

    added pseudo-classes and mediaqueries

--- src/common/Portfolio/Tile/styled.js
+++ src/common/Portfolio/Tile/styled.js
@@ -1,45 +1,79 @@
 import styled from "styled-components";
+import α from "color-alpha";
 
+export const List = styled.ul`
+    margin-top: 24px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    list-style: none;
+    padding: 0;
+    grid-gap: 32px;
 
-export const Section = styled.div`
-    border-radius: 4px;
-    background: ${({theme}) => theme.color.white};
-    box-shadow: 
-    0px 16px 58px 0px rgba(9, 10, 51, 0.03),
-    0px -2px 50px 0px rgba(9, 10, 51, 0.02);
+    @media(max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+        grid-template-columns: 1fr;
+        grid-gap: 24px;
+    }
+`;
+
+export const ListTile = styled.li`
     padding: 56px;
-    max-width: 600px;
-    border: 6px solid rgba(209, 213, 218, 0.3)
+    margin: 0;
+    background: ${({ theme }) => theme.colors.boxBackground};
+    border: 6px solid ${({ theme }) => theme.colors.tile.border};
+    transition: border-color 0.3s;
+    box-shadow: ${({ theme }) => theme.boxShadow};
+    border-radius: ${({ theme }) => theme.borderRadiusSmall};
 
+    &:hover {
+        border-color: ${({ theme }) => theme.colors.tile.borderHover};
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        padding: 20px;
+    }
 `;
 
 export const Title = styled.h3`
     font-weight: 700;
     font-size: 24px;
     letter-spacing: 2px;
-    color: ${({theme}) => theme.color.scienceblue};
+    color: ${({ theme }) => theme.color.scienceblue};
     margin: 0;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        padding: 20px;
+    }
 `;
 
 export const Description = styled.p`
     font-weight: 400;
     font-size: 18px;
     letter-spacing: 1px;
-    color: ${({theme}) => theme.color.slategray};
+    color: ${({ theme }) => theme.color.site.text};
     line-height: 25.2px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        margin-top: 16px;
+        font-size: 14px;
+    }
 `;
 
-export const Link = styled.div` 
+export const Link = styled.dl` 
     display: grid;
-    grid-template-rows: 1fr 1fr;
     grid-gap: 8px;
-    line-height: 25.2px;
+    line-height: 1.6;
+    grid-template-columns: 4em 1fr;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        margin-top: 16px;
+        font-size: 14px;
+    }
 `;
 
-export const LinkName = styled.div`
+export const LinkName = styled.dd`
     font-weight: 400px;
     font-size: 18px;
-    color: ${({theme}) => theme.color.slategray};
+    color: ${({ theme }) => theme.colors.textPrimary};
 `;
 
 export const LinkCode = styled.a`
@@ -48,7 +82,13 @@ export const LinkCode = styled.a`
     font-size: 18px;
     letter-spacing: 1px;
     word-break: break-all;
-    color: ${({theme}) => theme.color.scienceblue};
-    border-bottom: 1px solid ${({theme}) => theme.color.scienceblue};
+    color: ${({ theme }) => theme.colors.primary};
+    border-bottom: 1px solid ${({ theme }) => α(theme.color.primary, 0.3)};
+    padding-bottom: 1px;
     margin-left: 8px;
+
+    &:hover {
+        border-color: unset;
+    }
 `;
+
